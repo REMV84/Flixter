2015ff156782be557fd3e8e82986835ad21a943c
Ron <rhon.ventura@gmail.com>
2019-10-20 22:22:45 +0000

    permission

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,19 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_user_enrolled_in_course, only: [:show]
 
   def show
   end
 
+
+
   private
+
+  def require_user_enrolled_in_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled in the course!'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
